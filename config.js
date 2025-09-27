0bfe908d3894c95734c3fc1c322d5cf962948897
Lord Raghav <raghav@Shivangis-MacBook-Air.local>
2025-09-28 04:26:05 +0530

    vercel integration health check

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://admin_123:nhRb9NllTbzdEhuc@neweraproducts.wige5q7.mongodb.net/?retryWrites=true&w=majority&appName=NewEraProducts',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://admin_123:nhRb9NllTbzdEhuc@neweraproducts.wige5q7.mongodb.net/productsInventory?retryWrites=true&w=majority&appName=NewEraProducts',
   PORT: process.env.PORT || 3001,
   NODE_ENV: process.env.NODE_ENV || 'development',
   FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173'
